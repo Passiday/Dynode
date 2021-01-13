8cd9068b4753ea76e84aec85fd72b163f8cfcc3e
Deniss Fjodorovs <3starblaze@gmail.com>
2021-01-13 19:04:48 +0200

    Add recommended typescript ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'airbnb-base',
     'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
