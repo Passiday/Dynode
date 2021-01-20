d455e542d166cfbc4440b084c84c82a6edbdf8f0
Viesturs Spūlis <v.spuulis@gmail.com>
2021-01-21 01:52:00 +0200

    Update mathNode.ts

--- src/nodetypes/mathNode.ts
+++ src/nodetypes/mathNode.ts
@@ -5,10 +5,13 @@ import Node from '../node';
 
 class MathNode extends Node {
   operation: string; // Valid operations are: ["+", "-", "*", "/"]
+  
   def0: number|null;
+  
   def1: number|null;
+  
   constructor(operation: string, def0: number|null = null, def1: number|null = null) {
-    super(2, 1, "math_node");
+    super(2, 1, 'math_node');
     this.operation = operation;
     this.def0 = def0;
     this.def1 = def1;
@@ -16,10 +19,10 @@ class MathNode extends Node {
 
   update(): void {
     this.outputs[0].setValue(null); // Set default output to null
-    let value0 = (this.inputs[0].getValue() === null ? this.def0 : this.inputs[0].getValue());
-    let value1 = (this.inputs[1].getValue() === null ? this.def1 : this.inputs[1].getValue());
-    if(value0 !== null && value1 !== null) {
-      switch(this.operation) {
+    const value0 = (this.inputs[0].getValue() === null ? this.def0 : this.inputs[0].getValue());
+    const value1 = (this.inputs[1].getValue() === null ? this.def1 : this.inputs[1].getValue());
+    if (value0 !== null && value1 !== null) {
+      switch (this.operation) {
         case '+': // Addition
           this.outputs[0].setValue(value0 + value1);
           break;
@@ -30,8 +33,7 @@ class MathNode extends Node {
           this.outputs[0].setValue(value0 * value1);
           break;
         case '/': // Division
-          if(value1 === 0) // Output default null value when dividing by 0
-            break;
+          if (value1 === 0) break; // Output default null value when dividing by 0
           this.outputs[0].setValue(value0 / value1);
           break;
         default:
