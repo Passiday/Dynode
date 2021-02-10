cba53b6051572685a1e8e12b5b1c73e09e093e13
Pavils Jurjans <passiday@gmail.com>
2021-02-10 10:56:32 +0200

    Add type definitions for SVGBuilder. Add some demo code.

---
+++ src/SVGBuilder.d.ts
@@ -0,0 +1,132 @@
+type SVGElementAttrs = {[name: string]: any};
+
+export interface SVGElement {
+  new(): SVGElement;
+  element: HTMLElement;
+  debugMode: boolean;
+  log(...args: any[]): void;
+  createElement(elementName: string, attr: SVGElementAttrs, nodeValue: string): HTMLElement;
+  wrapElement(targetNode: HTMLElement): void;
+  setAttributes(attr: SVGElementAttrs, force?: boolean): void;
+  setStyle(styleObj: {[name: string]: string}, wipe?: boolean): void;
+  setValue(nodeValue: string): void;
+  init(elementName: string, attr: SVGElementAttrs, attrList: [string], nodeValue?: string): void;
+  insert(container: HTMLElement|null, wipe?: boolean): void;
+  remove(): void;
+  clone(): void;
+  getTransform(template?: string): SVGTransform;
+  translate(x: number, y: number, relative?: boolean): void;
+  rotate(angle: number, cx: number, cy: number, relative?: boolean): void;
+  scale(sx: number, sy: number): void;
+  setClip(clipPathId: string): void;
+  lowerToBottom(): void;
+  lower(): void;
+  raise(): void;
+  raiseToTop(): void;
+}
+
+export interface SVGTransform {
+  new(targetNode: HTMLElement, template?: string): SVGTransform;
+  addDefinition(fn: string, params: [number], apply?: boolean): void;
+  clear(apply?: boolean): void;
+  apply(): void;
+}
+
+export interface SVGRect extends SVGElement {
+  new(attr: SVGElementAttrs): SVGRect;
+}
+
+export interface SVGCircle extends SVGElement {
+  new(attr: SVGElementAttrs): SVGCircle;
+}
+
+export interface SVGEllipse extends SVGElement {
+  new(attr: SVGElementAttrs): SVGEllipse;
+}
+
+export interface SVGLine extends SVGElement {
+  new(attr: SVGElementAttrs): SVGLine;
+}
+
+export interface SVGPolyline extends SVGElement {
+  new(attr: SVGElementAttrs, points?: [{[string]: number|string}]): SVGPolyline;
+  addPoint(point: {[string]:number|string}): void;
+  update(): void;
+  clone(): SVGPolyline;
+  wrapElement(targetNode: HTMLElement): void;
+}
+
+export interface SVGPolygon extends SVGPolyline {
+  new(attr: SVGElementAttrs, points?: [{[string]:number|string}]): SVGPolygon;
+}
+
+export interface SVGPath extends SVGElement {
+  new(attr: SVGElementAttrs, points?: [{[string]:number|string}]): SVGPath;
+  addPoint(point: {[string]:number|string}): void;
+  update(): void;
+  clone(): SVGPath;
+  wrapElement(targetNode: HTMLElement): void;
+}
+
+export interface SVGText extends SVGElement {
+  new(attr: SVGElementAttrs, value: string): SVGText;
+}
+
+export interface SVGUse extends SVGElement {
+  new(attr: SVGElementAttrs, href: string): SVGUse;
+  setHref(href: string): void;
+}
+
+export interface SVGImage extends SVGElement {
+  new(attr: SVGElementAttrs, imgURL: string): SVGImage;
+  setHref(href: string): void;
+  load(onLoaded?: function, onError?: function): void;
+}
+
+export interface SVGFile extends SVGElement {
+  new(attr: SVGElementAttrs, svgURL: string): SVGFile;
+  load(onLoaded?: function, onError?: function): void;
+  getElement(id: string): SVGElement;
+}
+
+export interface SVGContainer extends SVGElement {
+  new(): SVGContainer;
+  addRect(attr: SVGElementAttrs): SVGRect;
+  addCircle(attr: SVGElementAttrs): SVGCircle;
+  addEllipse(attr: SVGElementAttrs): SVGEllipse;
+  addLine(attr: SVGElementAttrs): SVGLine;
+  addPolyline(attr: SVGElementAttrs, points?: [{[string]:number|string}]): SVGPolyline;
+  addPolygon(attr: SVGElementAttrs, points?: [{[string]:number|string}]): SVGPolygon;
+  addPath(attr: SVGElementAttrs, points?: [{[string]:number|string}]): SVGPath;
+  addText(attr: SVGElementAttrs, value: string): SVGText;
+  addGroup(attr: SVGElementAttrs): SVGGroup;
+  addDefs(attr: SVGElementAttrs): SVGDefs;
+  addUse(attr: SVGElementAttrs, href: string): SVGUse
+  addClipPath(attr: SVGElementAttrs): SVGClipPath;
+  addImage(attr, imgURL: string, onLoaded?: function, onError?: function): SVGImage;
+  addSVGFile(attr, svgURL: string, onLoaded?: function, onError?: function): SVGFile;
+}
+
+export interface SVGBuilder extends SVGContainer {
+  new(attr?:SVGElementAttrs): SVGBuilder;
+  setViewBox(params: {[string]:number|string}): void;
+  draggable(handle: SVGElement, callback?: function, body?: SVGElement): void;
+}
+
+export interface SVGGroup extends SVGContainer {
+  new(attr: SVGElementAttrs): SVGGroup;
+}
+
+export interface SVGClipPath extends SVGContainer {
+  new(attr: SVGElementAttrs): SVGClipPath;
+}
+
+export interface SVGDefs extends SVGContainer {
+  new(attr: SVGElementAttrs): SVGDefs;
+}
+
+/*
+TODO:
+export interface SVGSprite
+export interface SVGSpriteInstance extends SVGGroup
+*/
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,30 @@ import Node from './node';
 
 const n = new Network();
 
+import {SVGBuilder} from "./SVGBuilder";
+declare var SVGBuilder: SVGBuilder;
+
+function SVGPlayground() {
+  const svg = new SVGBuilder();
+  svg.insert(document.getElementById('dynodeContainer'));
+  const r = svg.addRect({x:10, y:10, width:200, height:150, style:{fill:'yellow'}});
+  r.translate(200, 100);
+  const p = svg.addPath({style:{fill:'red'}});
+  p.addPoint({cmd:'M', x:0, y:0});
+  p.addPoint({cmd:'l', dx:100, dy:20});
+  p.addPoint({cmd:'l', dx:-40, dy:50});
+  p.addPoint({cmd:'Z'});
+  p.update();
+  p.translate(400, 200);
+  svg.draggable(r);
+  svg.draggable(p);
+  return svg;
+}
+
+const sb = SVGPlayground();
+
 declare global { function publishToGlobal(assets: Record<string, unknown>):void; }
-global.publishToGlobal({
+publishToGlobal({
   demoNetwork: n,
+  svgBuilder: sb
 });
