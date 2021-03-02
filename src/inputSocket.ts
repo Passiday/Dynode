d0fa07e0af9652bbb5ec7c7396d940bd484e7a12
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-02 08:29:16 +0200

    Document inputSocket members

--- src/inputSocket.ts
+++ src/inputSocket.ts
@@ -2,6 +2,10 @@ import OutputSocket from './outputSocket';
 import Socket from './socket';
 import { VEventHandler, VEvent } from './vanillaEvent';
 
+
+/**
+ * Socket class that handles inputs.
+ */
 class InputSocket extends Socket {
   /**
    * Value used if the socket doesn't have a socket linked to it.
@@ -13,10 +17,19 @@ class InputSocket extends Socket {
    */
   defaultNothing = false;
 
+  /**
+   * Marks whether socket can have a default value.
+   */
   hasDefault = false;
 
+  /**
+   * Reference to the socket that is connected to the input.
+   */
   linkedSocket: OutputSocket | undefined;
 
+  /**
+   * Function that handles setting the value.
+   */
   valueHandler: VEventHandler | undefined;
 
   /**
@@ -34,25 +47,42 @@ class InputSocket extends Socket {
     this.hasDefault = true;
   }
 
+  /**
+   * Receive the default value that is set on the object.
+   */
   getDefaultValue(): unknown {
     if (!this.hasDefault) throw Error('Input socket has no default value set');
     if (this.defaultNothing) throw Error('Input socket default is nothing');
     return this.defaultValue;
   }
 
+  /**
+   * Check whether object's default value is set to nothing.
+   */
   isDefaultNothing() : boolean {
     if (!this.hasDefault) throw Error('Input socket has no default value set');
     return this.defaultNothing;
   }
 
+  /**
+   * Unset object's default value.
+   */
   clearDefault(): void {
     this.hasDefault = false;
   }
 
+  /**
+   * Check if the object's state is valid.
+   */
   isValid(): boolean {
     return this.linkedSocket ? true : this.hasDefault;
   }
 
+  /**
+   * Connect an OutputSocket object to the object.
+   *
+   * @param socket  An object that will be connected.
+   */
   linkSocket(socket: OutputSocket): void {
     this.linkedSocket = socket;
     this.valueHandler = (e: VEvent) => {
@@ -67,6 +97,9 @@ class InputSocket extends Socket {
     this.linkedSocket.addEventListener('value', this.valueHandler);
   }
 
+  /**
+   * Unlink the connected socket.
+   */
   clearLink(): void {
     if (this.linkedSocket === undefined) throw Error('linkedSocket is undefined');
     if (this.valueHandler === undefined) throw Error('valueHandler is undefined');
@@ -74,11 +107,17 @@ class InputSocket extends Socket {
     this.linkedSocket = undefined;
   }
 
+  /**
+   * Ask {@link linkedSocket} for output.
+   */
   pull(): void {
     if (!this.linkedSocket) throw new Error('Input socket is not linked.');
     this.linkedSocket.pull();
   }
 
+  /**
+   * Receive object's stored value.
+   */
   getValue(): unknown {
     if (this.linkedSocket) {
       return super.getValue();
@@ -86,6 +125,9 @@ class InputSocket extends Socket {
     return this.getDefaultValue();
   }
 
+  /**
+   * Check if object has a set value.
+   */
   isSet(): boolean {
     if (this.linkedSocket) {
       return super.isSet();
@@ -93,6 +135,9 @@ class InputSocket extends Socket {
     return this.hasDefault;
   }
 
+  /**
+   * Check if the object's value is set to nothing.
+   */
   isNothing(): boolean {
     if (this.linkedSocket) {
       return super.isNothing();
