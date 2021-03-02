1e52b465ee8deaba9773ce457b61de0dc206377e
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-02 13:14:21 +0200

    Partially document vanillaEvent

--- src/exports.ts
+++ src/exports.ts
@@ -3,5 +3,9 @@ import InputSocket from './inputSocket';
 import OutputSocket from './outputSocket';
 import Node from './node';
 import Network from './network';
+import { VEventTarget, VEvent, VEventHandler } from './vanillaEvent';
 
-export { Socket, InputSocket, OutputSocket, Node, Network };
+export {
+    Socket, InputSocket, OutputSocket, Node, Network,
+    VEventTarget, VEvent, VEventHandler,
+};
