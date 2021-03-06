3dd5019327d944663fc46491e52a97faf3dc625a
3starblaze <47479439+3starblaze@users.noreply.github.com>
2021-03-06 18:03:31 +0200

    More documentation (#17)
    * Document inputSocket members
    
    * Bundle up classes in 1 file for documentation
    
    The output before wasn't as pretty, sometimes class names were called
    "default" and constructor documentation wasn't showing up, plus it's
    giving more fine grained control over what to include in documentation.
    
    * Document outputSocket
    
    * Document Socket
    
    * Document Node
    
    * Document Network
    
    * Partially document vanillaEvent
    
    * Resolve linter issues

--- src/outputSocket.ts
+++ src/outputSocket.ts
@@ -1,27 +1,50 @@
 import Socket from './socket';
 import type Node from './node';
 
+/**
+ * Socket class that handles output.
+ */
 class OutputSocket extends Socket {
+  /**
+   * Mark whether the object is awaiting output.
+   */
   waiting = false;
 
+  /**
+   * Reference to a node that is responsible for this object.
+   */
   parent: Node;
 
+  /**
+   * @param parentNode  See {@link parent}
+   */
   constructor(parentNode: Node) {
     super();
     this.parent = parentNode;
   }
 
+  /**
+   * Ask {@link parent} to resolve.
+   */
   pull(): void {
     if (this.waiting) return;
     this.waiting = true;
     this.parent.resolve();
   }
 
+  /**
+   * Initialize the socket.
+   */
   init(): void {
     super.init();
     this.waiting = false;
   }
 
+  /**
+   * Object's setter.
+   *
+   * @param value  The new value. If omitted, value is set to nothing.
+   */
   setValue(value?: unknown): void {
     this.waiting = false;
     if (arguments.length) {
