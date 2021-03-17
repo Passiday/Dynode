d0173c15f1f50b8214ef9b462a4f15f62f347355
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-17 10:12:28 +0200

    Complete documentation for SocketCollection

--- src/socketCollection.ts
+++ src/socketCollection.ts
@@ -1,5 +1,8 @@
 import Socket from './socket';
 
+/**
+ * Class that handles related socket management.
+ */
 class SocketCollection {
   /**
    * A dictionary of sockets.
@@ -15,6 +18,11 @@ class SocketCollection {
    */
   private socketsOrder: string[] = [];
 
+  /**
+   * Register a socket to the collection.
+   *
+   * @param socket  Socket that will be added.
+   */
   public addSocket(socket: Socket): void {
     if (socket.accessName === null) {
       throw Error('socket needs an accessName to be added!');
@@ -23,10 +31,22 @@ class SocketCollection {
     this.socketsOrder.push(socket.accessName);
   }
 
+  /**
+   * Retrieve a socket from the collection, given its name.
+   *
+   * @param name  Socket's name in the collection.
+   * @return  A socket that corresponds to `name`.
+   */
   public getSocketByName(name: string): Socket {
     return this.socketsObject[name];
   }
 
+  /**
+   * Retrieve a socket from the collection, given its index.
+   *
+   * @param idx  Socket's index in the collection.
+   * @return  A socket that corresponds to `idx`.
+   */
   public getSocketByIndex(idx: number): Socket {
     return this.socketsObject[this.socketsOrder[idx]];
   }
