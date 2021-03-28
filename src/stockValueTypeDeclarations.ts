7c3c000cb967c04679c777a5105e915412d94779
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-28 12:47:05 +0300

    Add remaining primitive value types

--- src/stockValueTypeDeclarations.ts
+++ src/stockValueTypeDeclarations.ts
@@ -3,5 +3,5 @@ import ValueType from './valueType';
 export default [
   new ValueType('number', (value: unknown) => typeof value === 'number'),
   new ValueType('boolean', (value: unknown) => typeof value === 'boolean'),
-  // And rest of them
+  new ValueType('string', (value: unknown) => typeof value === 'string'),
 ];
