dbb049d904b65a276af2658bea7e9526d9d8d375
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-28 12:56:53 +0300

    Test Engine.getValueTypeDefinition

---
+++ tests/engine.test.ts
@@ -0,0 +1,30 @@
+import Engine from '../src/engine';
+import OutputSocket from '../src/outputSocket';
+
+describe('getValueTypeDefinition', () => {
+  test('Ensure number type exists', () => {
+    const e = new Engine();
+    expect(() => e.getValueTypeDefinition('string')).not.toThrow();
+  });
+
+  test('Ensure boolean type exists', () => {
+    const e = new Engine();
+    expect(() => e.getValueTypeDefinition('boolean')).not.toThrow();
+  });
+
+  test('Ensure string type exists', () => {
+    const e = new Engine();
+    expect(() => e.getValueTypeDefinition('string')).not.toThrow();
+  });
+
+  test('Ensure unknown type throws', () => {
+    const e = new Engine();
+    expect(() => e.getValueTypeDefinition('Object')).toThrow();
+  });
+
+  test('Ensure types are case sensitive', () => {
+    const e = new Engine();
+    expect(() => e.getValueTypeDefinition('string')).not.toThrow();
+    expect(() => e.getValueTypeDefinition('String')).toThrow();
+  })
+});
