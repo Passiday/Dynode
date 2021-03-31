a93e59acc62e7ef1a36fd422933f178d16a67e6c
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-31 15:23:45 +0300

    Create NodeType

--- src/stockNodeTypeDeclarations.ts
+++ src/stockNodeTypeDeclarations.ts
@@ -1,6 +1,35 @@
 import Node from './node';
 import type Engine from './engine';
 
+interface NodeCreatorFunc {
+  (name: string, engine?: Engine): Node;
+}
+
+class Nodetype {
+  private innerName;
+
+  /**
+   * @param name See {@link name}
+   * @param createFunc See {@link createFunc}
+   */
+  constructor(name: string, createFunc: NodeCreatorFunc) {
+    this.innerName = name;
+    this.createFunc = createFunc;
+  }
+
+  /**
+   * String representation of the node type.
+   */
+  get name(): string {
+    return this.innerName;
+  }
+
+  /**
+   * Function for creating a Node that matches this type.
+   */
+  readonly createFunc: NodeCreatorFunc;
+}
+
 function createMathNode(name: string, engine?: Engine): Node {
   const node = new Node(name, engine);
   node.addInput('operator', 'string');
@@ -49,4 +78,7 @@ function createConstNode(name: string, engine?: Engine): Node {
   return node;
 }
 
-export { createMathNode, createConstNode };
+export default [
+  new NodeType('math', createMathNode),
+  new NodeType('const', createConstNode),
+];
