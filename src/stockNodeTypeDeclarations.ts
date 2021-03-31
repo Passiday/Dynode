3a2755a8c993ab3ac59388d2324830dd69111d22
Deniss Fjodorovs <3starblaze@gmail.com>
2021-03-31 15:35:16 +0300

    Move NodeType to a separate file
    So that we can use the type in engine.

--- src/stockNodeTypeDeclarations.ts
+++ src/stockNodeTypeDeclarations.ts
@@ -1,29 +1,5 @@
 import Node from './node';
-
-class NodeType {
-  private innerName;
-
-  /**
-   * @param name See {@link name}
-   * @param createFunc See {@link createFunc}
-   */
-  constructor(name: string, createFunc: (node: Node) => Node) {
-    this.innerName = name;
-    this.createFunc = createFunc;
-  }
-
-  /**
-   * String representation of the node type.
-   */
-  get name(): string {
-    return this.innerName;
-  }
-
-  /**
-   * Function for creating a Node that matches this type.
-   */
-  readonly createFunc: (node: Node) => Node;
-}
+import NodeType from './nodeType';
 
 function createMathNode(node: Node): Node {
   const n = node;
