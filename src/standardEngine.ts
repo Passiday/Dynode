4f2b9794e90fc8a7cc49cea71de402ee4841693f
Deniss Fjodorovs <3starblaze@gmail.com>
2021-04-09 18:47:25 +0300

    Rename ..TypeDeclaration to ..TypeDefinition
    Initially it was "definition" but for some reason I wrote them
    "declaration" at some places and I believe the naming has to be consistent.

--- src/standardEngine.ts
+++ src/standardEngine.ts
@@ -1,14 +1,14 @@
 import Engine from './engine';
-import StockValueTypeDeclarations from './stockValueTypeDeclarations';
-import StockNodeTypeDeclarations from './stockNodeTypeDeclarations';
+import StockValueTypeDefinitions from './stockValueTypeDefinitions';
+import StockNodeTypeDefinitions from './stockNodeTypeDefinitions';
 
 class StandardEngine extends Engine {
   constructor() {
     super();
-    for (const nodeType of StockNodeTypeDeclarations) {
+    for (const nodeType of StockNodeTypeDefinitions) {
       this.addNodeTypeDefinition(nodeType);
     }
-    for (const valueType of StockValueTypeDeclarations)
+    for (const valueType of StockValueTypeDefinitions)
       this.addValueTypeDefinition(valueType);
   }
 }
