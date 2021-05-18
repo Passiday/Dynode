ed36ce8012b717895127225b75a2ef61b4635248
Roberts Cerins <cerins4141@gmail.com>
2021-05-18 15:51:23 +0300

    Fix

--- src/Dynode/controller/node.ts
+++ src/Dynode/controller/node.ts
@@ -41,7 +41,7 @@ export default class NodeController {
     this.model.addEventListener('afterResolve', afterResolve);
     this.model.addEventListener('nodeRemoved', nodeRemoved); // Perhaps this event belongs to the Network model?
     // TODO Handle "nothing" in inputChange
-    this.view.addEventListener('inputChange', (ev: VEvent) => {
+    this.view.addEventListener('inputChange', (ev: VEvent<NodeView>) => {
       for (const [key, value] of Object.entries(ev.detail as Record<string, unknown>)) {
         node.getInput(key).setDefaultValue(value);
       }
